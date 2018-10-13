a2e7e86687618a0537db27c4514ff782a3f6fb8a
Milan Dian <dian.milan@seznam.cz>
2018-10-14 00:15:11 +0200

    new 002

---
+++ Work-Img-Sprites-map/map5_vnitrek1v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map5_vnitrek1v2" tilewidth="16" tileheight="16" tilecount="286" columns="13">
+ <image source="../images/map5_vnitrek1v2.png" width="219" height="361"/>
+</tileset>
